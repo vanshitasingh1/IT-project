29a513ba04c1ae7adbf66399c5b0728f3a3ff316
Bhawna Sharma <sharmaji9903@gmail.com>
2021-10-05 19:50:58 +0530

    DHANVANTRI

---
+++ app/src/main/java/com/example/dhanvantri/MainActivity1.java
@@ -0,0 +1,54 @@
+package com.example.dhanvantri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    String TAG ="MainActivity1" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"--onCreate--");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "--onStart--");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "--onRestart--");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "--onPause--");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "--onResume--");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "--onStop--");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "--onDestroy--");
+    }
+}
