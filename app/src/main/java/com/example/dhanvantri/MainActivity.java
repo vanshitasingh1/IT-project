25aeb18ca0ff4ee64c8517c1f0ec4991e91b41c1
Bhawna Sharma <79350994+Bhawna-Sharma-9903@users.noreply.github.com>
2021-10-11 19:01:15 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/dhanvantri/MainActivity.java
+++ app/src/main/java/com/example/dhanvantri/MainActivity.java
@@ -3,12 +3,63 @@ package com.example.dhanvantri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    String TAG ="MainActivity1" ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button letSTART= (Button) findViewById(R.id.button5);
+        letSTART.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Dhanvantri is activated");
+            }
+        });
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"--onCreate--");
+    }
+
+    private void getDhanvantri_is_activated(Button letSTART) {
+        }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "--onStart--");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "--onRestart--");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "--onPause--");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "--onResume--");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "--onStop--");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "--onDestroy--");
     }
 }
+
